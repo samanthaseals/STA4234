598c26cda71ef182d724ff1007b2ed6afce575d1
Samantha Seals <sseals@uwf.edu>
2021-10-25 23:14:48 -0500

    add multinomial logistic regression example

---
+++ C22 - multinomial logistic regression.R
@@ -0,0 +1,30 @@
+library(gsheet)
+library(tidyverse)
+library(nnet)
+library(AER)
+
+# pull in data
+data <- as_tibble(gsheet2tbl("https://docs.google.com/spreadsheets/d/1T2JvJY2IG9fH8Q26RvLV6MM9ed5f8EbECGRnt0aZQ2M/edit?usp=sharing"))
+
+# set order of the outcome
+# we want the "other" category to be the reference group, so it needs to be "first"
+data$food <- factor(data$food, levels = c("O", "I", "F"))
+
+# use the multinom() function to construct the multinomial logistic regression
+m1 <- multinom(food ~ length, data = data)
+
+# summary, which includes all models constructed
+summary(m1)
+
+# look at overall significance (use partial F test)
+# saying outcome ~ 1 gives us the intercept-only model
+m2 <- multinom(food ~ 1, data = data)
+anova(m2, m1)
+
+# get test for significant predictors out using coeftest()
+coeftest(m1)
+
+# 95% confidence intervals for model terms
+confint(m1)
+
+
