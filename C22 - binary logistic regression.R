168ee5f560949048e35ae84f58feb1fbdab4a112
Samantha <sseals@uwf.edu>
2021-11-01 15:30:59 -0500

    add linear regression comparison

--- C22 - binary logistic regression.R
+++ C22 - binary logistic regression.R
@@ -1,9 +1,25 @@
 library(gsheet)
 library(tidyverse)
 
+# define function to graph ANOVA assumptions
+# written by previous student Reid Ginoza
+almost_sas <- function(aov.results){
+  aov_residuals <- residuals(aov.results)
+  par(mfrow=c(2,2))
+  plot(aov.results, which=1)
+  hist(aov_residuals)
+  plot(aov.results, which=2)
+  plot(density(aov_residuals))
+}
+
 data <- as_tibble(gsheet2tbl("https://docs.google.com/spreadsheets/d/1fCIhZTf4BnE_Xly4zp8Cg_cz4wAYrQN0WPN9vDnqSEE/edit#gid=0"))
 
 data$rank <- as_factor(data$rank)
+
+# try regression using normal distribution
+m1 <- lm(admit ~ gre + gpa + rank, data = data)
+summary(m1)
+almost_sas(m1)
 
 # logistic regression is done with the glm() function
 # we specify family = "binomial" to tell R that it's a 0/1 outcome
